18bc3965dde1eb9bf0acab7daf6565a9dbb4920a
Abhinav <abhinav.maurya202@gmail.com>
2022-05-11 16:13:21 +0530

    Container With Most Water Done

--- Arrays/ContainerMostWater.cpp
+++ Arrays/ContainerMostWater.cpp
@@ -1,3 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int start = 0;
+        int end = height.size() - 1;
+        int area = (end - start)*(min(height[start], height[end]));
+        while (start < end)
+        {
+            if (height[start] < height[end])
+            {
+                start++;
+                area = max(area,(end - start)*(min(height[start], height[end])));
+            }
+            else
+            {
+                end--;
+                area = max(area,(end - start)*(min(height[start], height[end])));
+            }
+        }
+        return area;
+    }
+};
