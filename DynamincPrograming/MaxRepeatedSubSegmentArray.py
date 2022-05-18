14fbe4575206c8ff3ac339b91c0e190c9144f143
Abhinav <abhinav.maurya202@gmail.com>
2022-05-18 16:46:22 +0530

    Max Repeated SubSegment Array.pyDone

---
+++ DynamincPrograming/MaxRepeatedSubSegmentArray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findLength(self, A, B):
+        dp = [[0 for _ in range(len(B) + 1)] for _ in range(len(A) + 1)]
+        ans = 0
+        for i in range(1, len(A) + 1):
+            for j in range(1, len(B) + 1):
+                if A[i - 1] == B[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                    ans = max(ans,dp[i][j])
+        return ans
